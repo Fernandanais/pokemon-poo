91268a032e05a788c3f9c959321bd7d4af712304
Fernanda Zamora <fernandanais@gmail.com>
2017-05-04 11:44:42 -0300

    Agregamos los selectores y los otros pokemones

--- js/main.js
+++ js/main.js
@@ -16,11 +16,12 @@ function Pokemon(nombre, color, poderDelAtaque){
 		pokemon.vida = pokemon.vida - this.poderDelAtaque
 	}
 }
-
+const Tangananica = new Pokemon('Tangananica', 'verde', 80);
+const Tanganana = new Pokemon('Tanganana', 'azul', 40);
 const Pikachu = new Pokemon('Pikachu', 'amarillo', 100);
 const Charmander = new Pokemon("Charmander", "rojo", 20);
 
 Pikachu.atacar(Charmander)
 console.log(Charmander.vida);
-document.write("La cantidad de vidas que le quedan a Charmander son: "+Charmander.vida);
+document.write("<br>La cantidad de vidas que le quedan a Charmander son: "+Charmander.vida);
 
