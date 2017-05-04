3d3ac140048b53b5610e7fcc7519acdd89bac5dc
Fernanda Zamora <fernandanais@gmail.com>
2017-05-04 11:05:43 -0300

    Agregamos los archivos

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function Pokemon(nombre, color, poderDelAtaque){
+	this.nombre = nombre,
+	this.color = color,
+	this.nivelDeAmistad = 0,
+	this.vida = 100,
+	this.poderDelAtaque = poderDelAtaque,
+
+	this.mostrarPokemon = function(){
+	return ("Hola soy: "+this.nombre+" y soy de color: "+this.color)
+}
+	this.aumentarAmistad = function(valor){
+		this.nivelDeAmistad = this.nivelDeAmistad + valor
+	}
+
+	this.atacar = function(pokemon){
+		pokemon.vida = pokemon.vida - this.poderDelAtaque
+	}
+}
+
+const Pikachu = new Pokemon('Pikachu', 'amarillo', 100);
+const Charmander = new Pokemon("Charmander", "rojo", 20);
+
+Pikachu.atacar(Charmander)
+console.log(Charmander.vida);
+document.write("La cantidad de vidas que le quedan a Charmander son: "+Charmander.vida);
+
